3a24eb11ebe6b52cff61e0fed204d5fbefb5f6c8
Armin Veres <armin.veres@hotmail.com>
2023-12-16 17:41:37 +0100

    refactor: nicer prints and move to single set call

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
+/// Adapted from: https://github.com/DriftKingTW/Raspberry-Pi-PWM-Fan-Control
+/// and from the RPPAL examples
 use std::{error::Error, fs, thread, time::Duration};
 
 use rppal::pwm::{Channel, Polarity, Pwm};
-
-/// Adjusted from: https://github.com/DriftKingTW/Raspberry-Pi-PWM-Fan-Control
 
 const PWM_FREQ: f64 = 25_000.;
 const PWM_DUTY_CYCLE: f64 = 0.5;
@@ -17,19 +17,18 @@ const MAX_TEMP: f64 = 70.;
 const FAN_LOW: f64 = 0.1;
 const FAN_HIGH: f64 = 1.;
 const FAN_OFF: f64 = 0.;
-// const FAN_MAX: f64 = 1.;
+// gain curve, this is linear now
 const FAN_GAIN: f64 = (FAN_HIGH - FAN_LOW) / (MAX_TEMP - MIN_TEMP);
 
 fn get_cpu_temp() -> f64 {
     let temp_file = fs::read_to_string("/sys/class/thermal/thermal_zone0/temp")
         .expect("Thermal zone was not found");
     let mut temp_val = temp_file.trim().parse::<f64>().expect("Didn't parse temp");
+    // Convert to Degress from Millidegrees
     temp_val /= 1000.;
+    println!("Temperature at : {temp_val:.1} C");
     temp_val
 }
-
-// fn handle_temp() {
-// }
 
 fn main() -> Result<(), Box<dyn Error>> {
     let pwm = Pwm::with_frequency(
@@ -41,16 +40,18 @@ fn main() -> Result<(), Box<dyn Error>> {
     )?;
 
     loop {
-        // Sleep for 1 seconds while for lower usage
         let temp = get_cpu_temp();
+        let mut dc = 0.;
         if temp > MIN_TEMP {
             let delta = temp.min(MAX_TEMP) - MIN_TEMP;
-            pwm.set_duty_cycle(FAN_LOW + delta * FAN_GAIN)?;
+            dc = FAN_LOW + delta * FAN_GAIN;
         } else if temp < OFF_TEMP {
-            pwm.set_duty_cycle(FAN_OFF)?;
+            dc = FAN_OFF;
         }
+        println!("Setting duty cycle to: {dc:.2}!");
+        pwm.set_duty_cycle(dc)?;
+
+        // Sleep for 1 seconds while for lower usage
         thread::sleep(Duration::from_secs(1));
     }
-
-    Ok(())
 }
